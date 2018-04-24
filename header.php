8db34e753132c9632e67cb1cd91886785cbf8ce3
Max Longton <elongton@vt.edu>
2018-04-24 14:49:04 -0400

    updated featured event section

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
--- template-parts/homepage/content-featured_event_section.php
+++ template-parts/homepage/content-featured_event_section.php
@@ -1,28 +1,40 @@
-<div class="" style="padding: 50px; background-color: #006599;">
+<div class="" style="padding: 50px 0; background-color: #006599;">
 
-<div class="container" style="display: flex;">
+  <!-- <div class="container" style="margin-bottom: 50px;">
+      <div class="center-content">
+          <div class="row">
+              <div class="col-md-6 col-md-offset-3">
+                  <h2 class="section-title" style="color: white;">Featured</h2>
+                  <span class="underline center"></span>
+              </div>
+          </div>
+      </div>
+  </div> -->
 
-  <img style="border-radius: 7px;"  class="img-fluid" src="<?php echo get_parent_theme_file_uri(); ?>/assets/images/customization/homepage/featured_event/girl.png" alt="">
+  <div class="container">
+    <div class="row featuredevent_row">
+      <div class="col-md-6 featuredevent_image_div">
+        <div class="" style="width: 100%;">
+            <img style="border-radius: 7px;"  style="width: 100%;" src="<?php echo get_parent_theme_file_uri(); ?>/assets/images/customization/homepage/featured_event/girl.png" alt="">
+        </div>
 
-  <div class="" style="color: white; padding-left: 30px; padding-right: 15px; display: flex; flex-direction: column;">
-    <div class="" style="">
-      <!-- <span style="font-size: 30px;">Richmond Art Fair</span><br> -->
-      <h2>Richmond Art Fair</h2><br>
-      <span style="font-size: 20px;">May 1st - May 25th</span><br>
-      <span style="font-size: 20px;">Belmont Branch Library</span>
-      <p style="font-size: 18px; margin-top: 15px; line-height: 30px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dignissim aliquam lectus id suscipit. Integer accumsan lorem faucibus mi rhoncus, ut egestas diam tempus. Duis ut pulvinar dolor. Donec faucibus mi eu nisi malesuada, et sollicitudin eros sollicitudin. Nullam massa nunc, imperdiet non iaculis vitae, tincidunt volutpat nisi. Proin luctus urna lectus, sed interdum odio dapibus ut. Curabitur pellentesque lectus nec lorem egestas fermentum. Aenean ac convallis eros, eleifend pretium justo.</p>
-    </div>
-    <div class="" style="flex-grow: 1; display: flex; align-items: center; justify-content: center;">
-      <a href="" style="margin-right: 10px;" class="btn btn-primary">Sign Up</a>
-      <a href="" class="btn btn-primary">View More Events</a>
-    </div>
+      </div>
 
-
-
-  </div>
-
-
-</div>
+      <div class="col-md-6 featuredevent_text_div" style="color: white; padding-right: 15px; display: flex; flex-direction: column;">
+        <div class="" style="">
+          <!-- <span style="font-size: 30px;">Richmond Art Fair</span><br> -->
+          <h2>Richmond Art Fair</h2><br>
+          <span style="font-size: 20px;">May 1st - May 25th</span><br>
+          <span style="font-size: 20px;">Belmont Branch Library</span>
+          <p style="font-size: 18px; margin-top: 15px; line-height: 30px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dignissim aliquam lectus id suscipit. Integer accumsan lorem faucibus mi rhoncus, ut egestas diam tempus. Duis ut pulvinar dolor. Donec faucibus mi eu nisi malesuada, et sollicitudin eros sollicitudin. Nullam massa nunc, imperdiet non iaculis vitae, tincidunt volutpat nisi. Proin luctus urna lectus, sed interdum odio dapibus ut. Curabitur pellentesque lectus nec lorem egestas fermentum. Aenean ac convallis eros, eleifend pretium justo.</p>
+        </div>
+        <div class="" style="flex-grow: 1; display: flex; align-items: center; justify-content: center;">
+          <a href="" style="margin-right: 10px;" class="btn btn-primary">Sign Up</a>
+          <a href="" class="btn btn-primary">View More Events</a>
+        </div>
+      </div>
+    </div><!--row-->
+  </div><!--container-->
 
 
 </div>
