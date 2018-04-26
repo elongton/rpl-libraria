f225179988e6f0a059105f1ce3d017af33e41685
Max Longton <elongton@vt.edu>
2018-04-26 10:42:40 -0400

    added tile control

--- template-parts/homepage/content-tiles.php
+++ template-parts/homepage/content-tiles.php
@@ -1,66 +1,85 @@
+<?php
+$tile1   =    get_field('tile1');
+$tile2   =    get_field('tile2');
+$tile3   =    get_field('tile3');
+$tile4   =    get_field('tile4');
+$tile5   =    get_field('tile5');
+$tile6   =    get_field('tile6');
+?>
+
+
+
+
 <section class="features">
     <div class="tiles">
         <ul>
-            <li class="bg-yellow">
-                <div class="feature-box">
-                    <i class="yellow"></i>
-                    <h3>Books</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dolor turpis, pulvinar varius dui id, convallis iaculis eros.</p>
-                    <a class="yellow" href="#">
-                        View Selection <i class="fa fa-long-arrow-right"></i>
-                    </a>
+          <a href="<?php echo $tile1['link_url']; ?>">
+            <li style="background-image: url('<?php echo $tile1['image']; ?>'); background-size: cover; background-repeat: no-repeat;">
+                <div class="feature-box" style="background-color: <?php echo $tile1['background_color']; ?>;">
+                  <div class="feature-box-darkness" style="border-radius: 10px; padding: 15px; height: inherit; width: inherit;">
+                    <i class="tile_icon <?php echo $tile1['icon']; ?>"></i>
+                    <h3><?php echo $tile1['title']; ?></h3>
+                    <p><?php echo $tile1['description']; ?></p>
+                  </div>
                 </div>
             </li>
-            <li class="bg-light-green">
-                <div class="feature-box">
-                    <i class="light-green"></i>
-                    <h3>eBooks</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dolor turpis, pulvinar varius dui id, convallis iaculis eros.</p>
-                    <a class="light-green" href="#">
-                        View Selection <i class="fa fa-long-arrow-right"></i>
-                    </a>
+          </a>
+          <a href="<?php echo $tile2['link_url']; ?>">
+            <li style="background-image: url('<?php echo $tile2['image']; ?>'); background-size: cover; background-repeat: no-repeat;">
+                <div class="feature-box" style="background-color: <?php echo $tile2['background_color']; ?>;">
+                  <div class="feature-box-darkness" style="border-radius: 10px; padding: 15px;">
+                    <i class="tile_icon <?php echo $tile2['icon']; ?>"></i>
+                    <h3><?php echo $tile2['title']; ?></h3>
+                    <p><?php echo $tile2['description']; ?></p>
+                  </div>
                 </div>
             </li>
-            <li class="bg-blue">
-                <div class="feature-box">
-                    <i class="blue"></i>
-                    <h3>DVDs</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dolor turpis, pulvinar varius dui id, convallis iaculis eros.</p>
-                    <a class="blue" href="#">
-                        View Selection <i class="fa fa-long-arrow-right"></i>
-                    </a>
+          </a>
+
+          <a href="<?php echo $tile3['link_url']; ?>">
+            <li style="background-image: url('<?php echo $tile3['image']; ?>'); background-size: cover; background-repeat: no-repeat;">
+                <div class="feature-box" style="background-color: <?php echo $tile3['background_color']; ?>;">
+                  <div class="feature-box-darkness" style="border-radius: 10px; padding: 15px;">
+                    <i class="tile_icon <?php echo $tile3['icon']; ?>"></i>
+                    <h3><?php echo $tile3['title']; ?></h3>
+                    <p><?php echo $tile3['description']; ?></p>
+                  </div>
                 </div>
             </li>
-            <li class="bg-red">
-                <div class="feature-box">
-                    <i class="red"></i>
-                    <h3>Magazines</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dolor turpis, pulvinar varius dui id, convallis iaculis eros.</p>
-                    <a class="red" href="#">
-                        View Selection <i class="fa fa-long-arrow-right"></i>
-                    </a>
+          </a>
+          <a href="<?php echo $tile4['link_url']; ?>">
+            <li style="background-image: url('<?php echo $tile4['image']; ?>'); background-size: cover; background-repeat: no-repeat;">
+                <div class="feature-box" style="background-color: <?php echo $tile4['background_color']; ?>;">
+                  <div class="feature-box-darkness" style="border-radius: 10px; padding: 15px;">
+                    <i class="tile_icon <?php echo $tile4['icon']; ?>"></i>
+                    <h3><?php echo $tile4['title']; ?></h3>
+                    <p><?php echo $tile4['description']; ?></p>
+                  </div>
                 </div>
             </li>
-            <li class="bg-violet">
-                <div class="feature-box">
-                    <i class="violet"></i>
-                    <h3>Audio</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dolor turpis, pulvinar varius dui id, convallis iaculis eros.</p>
-                    <a class="violet" href="#">
-                        View Selection <i class="fa fa-long-arrow-right"></i>
-                    </a>
+          </a>
+          <a href="<?php echo $tile5['link_url']; ?>">
+            <li style="background-image: url('<?php echo $tile5['image']; ?>'); background-size: cover; background-repeat: no-repeat;">
+                <div class="feature-box" style="background-color: <?php echo $tile5['background_color']; ?>;">
+                  <div class="feature-box-darkness" style="border-radius: 10px; padding: 15px;">
+                    <i class="tile_icon <?php echo $tile5['icon']; ?>"></i>
+                    <h3><?php echo $tile5['title']; ?></h3>
+                    <p><?php echo $tile5['description']; ?></p>
+                  </div>
                 </div>
             </li>
-            <li class="bg-green">
-                <div class="feature-box">
-                    <i class="green"></i>
-                    <h3>eAudio</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dolor turpis, pulvinar varius dui id, convallis iaculis eros.</p>
-                    <a class="green" href="#">
-                        View Selection <i class="fa fa-long-arrow-right"></i>
-                    </a>
+          </a>
+          <a href="<?php echo $tile6['link_url']; ?>">
+            <li style="background-image: url('<?php echo $tile6['image']; ?>'); background-size: cover; background-repeat: no-repeat;">
+                <div class="feature-box" style="background-color: <?php echo $tile6['background_color']; ?>;">
+                  <div class="feature-box-darkness" style="border-radius: 10px; padding: 15px;">
+                    <i class="tile_icon <?php echo $tile6['icon']; ?>"></i>
+                    <h3><?php echo $tile6['title']; ?></h3>
+                    <p><?php echo $tile6['description']; ?></p>
+                  </div>
                 </div>
             </li>
+          </a>
         </ul>
     </div>
 </section>
